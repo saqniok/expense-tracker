8df3a7a734f713fbe89a97d700c11e15a6d4eb49
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-06-19 10:10:37 +0200

    fixed realoding page, after delete/add expense

--- frontend/src/expenses-list/delete-button.js
+++ frontend/src/expenses-list/delete-button.js
@@ -3,10 +3,10 @@ import { showExpenses } from './show-expenses.js'
 import { showError } from '../ui-helpers.js'
 
 async function onDeleteButtonClicked(element, expense) {
-    const result = await removeExpense(expense.id); // Trying to delete an expense by its id
+    const result = await removeExpense(expense.id);         // Trying to delete an expense by its id
 
-    if(result.success) { await showExpenses(element); } // If the deletion was successful - update the interface
-    else { showError(element, result.error); } // Otherwise - show the error message in the interface
+    if(result.success) { await showExpenses(element); }     // If the deletion was successful - update the interface
+    else { showError(element, result.error); }              // Otherwise - show the error message in the interface
 }
 
 /**
@@ -23,16 +23,17 @@ export function appendDeleteButton(parent, element, expense) {
     // Create a caption on the button
     const confirmLabel = document.createElement('span');
     confirmLabel.classList.add('confirm-label');
-    //confirmLabel.textContent = 'X';  // initial text of the button
+    //confirmLabel.textContent = 'X';   // initial text of the button
 
     button.appendChild(confirmLabel);   // Add the caption inside the button
     parent.appendChild(button);         // Insert the button in the parent DOM element (for example, in <li>)
 
-    let confirmState = false; // Logical variable: whether the button requires confirmation
+    let confirmState = false;           // Logical variable: whether the button requires confirmation
 
     // Assign a button click handler. It will be called every time the user clicks on the delete button.
     // The handler will check whether the button requires confirmation or not and if it does - will show a confirmation dialog
     button.addEventListener('click', async () => {
+
     if (!confirmState) { // Check whether the confirmation mode (second click) is active now. If not, it means that this is the first click, and we need to ask the user for confirmation
         
         button.dataset.confirm = "true";    // visual trigger for CSS (changes text from X to Delete?)
@@ -51,6 +52,7 @@ export function appendDeleteButton(parent, element, expense) {
         button.disabled = true;
         try {
             await onDeleteButtonClicked(element, expense);
+            
         } catch (error) {
             button.disabled = false;
         }
